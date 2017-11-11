68ddd02bdfcfbf1057aef71b2816483e3b02b345
spkvn <kevinmeagher1@hotmail.com>
2017-11-11 22:57:06 +1100

    site nav change

--- resources/views/layouts/site/nav.blade.php
+++ resources/views/layouts/site/nav.blade.php
@@ -1,12 +1,13 @@
 <div class="top-bar">
     <div class="top-bar-left">
         <ul class="dropdown menu" data-dropdown-menu>
-            <li class="menu-text">{{config('app.name', "projexx")}}</li>
+            <li class="menu-text">vhom.org</li>
         </ul>
     </div>
     <div class="top-bar-right">
         <ul class="menu">
-            <li>Site Button</li>
+            <li><a href="{{route('home')}}">Projects</a></li>
+            <li><a href="{{route('login')}}">Admin</a></li>
         </ul>
     </div>
 </div>
