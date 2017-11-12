12528528158d462d704b4a1addb9cbc666eb4cfd
spkvn <kevinmeagher1@hotmail.com>
2017-11-12 12:17:02 +1100

    add tag & taggables

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace VhomHome\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    //
+}
---
+++ app/Tag.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace VhomHome;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    //
+}
