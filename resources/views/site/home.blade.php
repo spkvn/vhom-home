628a7f99e6a7d5215dba4b6edf5322043adf8811
spkvn <kevinmeagher1@hotmail.com>
2017-11-16 18:11:11 +1100

    add gh link to page

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -2,7 +2,10 @@
 @section('content')
     <div class="row expanded site__main__img">
         <div class="main__img__flex">
-            <h1>Kevin's Projects</h1>
+            <div>
+                <h1>Kevin's Projects</h1>
+                <p><a href="https://github.com/spkvn">Github</a></p>
+            </div>
         </div>
     </div>
 
@@ -36,26 +39,4 @@
             </div>
         @endforeach
     </div>
-    {{--<div class="row expanded site__main__img">--}}
-        {{--<div class="row content__wrapper">--}}
-            {{--<div class="row">--}}
-                {{--<div class="columns small-12">--}}
-                    {{--<h1>Kevin's Projects</h1>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-            {{--@foreach($projects as $project)--}}
-            {{--<div class="row project__row">--}}
-                {{--<div class="columns small-7">--}}
-                    {{--<h1>{{$project->title}}</h1>--}}
-                    {{--<p>{{$project->description}}</p>--}}
-                {{--</div>--}}
-                {{--<div class="columns small-5">--}}
-                    {{--<div class="project__image__container">--}}
-                        {{--<img class="project__image" src="{{asset($project->image_path)}}">--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-            {{--@endforeach--}}
-        {{--</div>--}}
-    {{--</div>--}}
 @endsection
